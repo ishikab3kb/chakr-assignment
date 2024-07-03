d2dd6646fe05114e0e9371506e7f9eab46720dad
Ishika Jain <ishikbb3ij11@gmail.com>
2024-07-04 04:41:15 +0530

    updated files empty appearance

--- src/app/settings/appearance/page.tsx
+++ src/app/settings/appearance/page.tsx
@@ -1,25 +1,25 @@
-// import * as React from "react"
-// import { LaptopMinimal, Moon, Sun } from "lucide-react"
-// import { useTheme } from "next-themes"
+import * as React from "react"
+import { LaptopMinimal, Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
  
-// import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 
  
-// export default function page() {
+export default function page() {
 //   const { setTheme } = useTheme();
  
-//   return (
+  return (
     
-//         <div>
-//             <Button onClick={() => setTheme("light")}>
-//           Light<Sun></Sun>
-//         </Button>
-//         <Button onClick={() => setTheme("dark")}>
-//           Dark<Moon></Moon>
-//         </Button>
-//         <Button onClick={() => setTheme("system")}>
-//           System<LaptopMinimal />
-//         </Button>
-//         </div>
-//   )
-// }
+        <div>
+            {/* <Button onClick={() => setTheme("light")}>
+          Light<Sun></Sun>
+        </Button>
+        <Button onClick={() => setTheme("dark")}>
+          Dark<Moon></Moon>
+        </Button>
+        <Button onClick={() => setTheme("system")}>
+          System<LaptopMinimal />
+        </Button> */}
+        </div>
+  )
+}
